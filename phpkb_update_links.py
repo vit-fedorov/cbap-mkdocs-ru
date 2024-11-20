f6d53889dddf029c69efeb15ad805cfd52c56116
Arterm Sedov <arterm.sedov@gmail.com>
2024-11-20 17:01:50 +0300

    Added phpkb_update_links.py

--- phpkb_update_links.py
+++ phpkb_update_links.py
@@ -1,18 +1,17 @@
 import mysql.connector
+# import sshtunnel
 from sshtunnel import SSHTunnelForwarder
 from getpass import getpass
+import html
 from html.parser import HTMLParser
 import bs4
 from markdownify import markdownify as md
+from markdownify import MarkdownConverter
 import re
 from pathvalidate import sanitize_filename
 from pathlib import Path
+import shutil
 from cryptography.fernet import Fernet
-
-class myparser(HTMLParser):
-	def handle_starttag(self, tag, attrs):
-		if tag == 'img':
-			print("IMG tag with attrs %s\n" % repr(attrs))
 
 import os
 
@@ -20,141 +19,135 @@ import os.path
 
 import json
 
-TOTAL_PAGES_CLONED = 0
+TOTAL_PAGES_UPDATED = 0
 CONNECTION = None
-
+# sshtunnel.SSH_TIMEOUT=0.5
+# sshtunnel.TUNNEL_TIMEOUT=20.0
 MAPPING = dict()
-CATEGORY_MAPPING = dict()
-ARTICLE_MAPPING = dict()
-# CATEGORY_COUNTER = 0
 
-def cloneCategoryChildren(parent, newParentId=''):
+
+def updateCategoryChildren(parent):
     c = CONNECTION.cursor(buffered=True)
     
     id = parent[0]
     title = parent[1]
     parentId = parent[2]
-    
-    if newParentId:
-        parentId = newParentId
-    
-    newCategoryId = cloneCategory(id, parentId)
-    CATEGORY_MAPPING.update({id:newCategoryId})
-    updateMappingJson()
-    
-    #for id, title, parent_id, in parent:
+  
     c.execute(f"""
         SELECT DISTINCT (category_id), category_name, parent_id
         FROM phpkb_categories 
-        WHERE category_show='yes' 
-        AND category_status = 'public'
+        WHERE 
+        -- category_show='yes' 
+        -- AND 
+        category_status = 'public'
         AND phpkb_categories.language_id = 2
         AND parent_id = {id}
         """)
     children = c.fetchall()
     print(f"\n-----\n\nCategory {id}. {title}. Children: {children}\n")
-    print(f'Cloning articles from Category {id}. {title}.')
-    cloneArticlesInCategory(id, newCategoryId)
+    print(f'Updating articles from Category {id}. {title}.')
+    updateArticlesInCategory(id)
     for child in children:
-        cloneCategoryChildren(child, newCategoryId)
+        updateCategoryChildren(child)
     return children
     
         
-def cloneArticlesInCategory (category_id, newCategoryId):
+def updateArticlesInCategory (category_id):
     c = CONNECTION.cursor(buffered=True)
     c.execute(f"""
             SELECT DISTINCT (phpkb_articles.article_id), phpkb_articles.article_content, phpkb_articles.article_title 
             FROM phpkb_articles, phpkb_relations, phpkb_categories 
-            WHERE article_show='yes' 
-            AND phpkb_relations.category_id = {category_id} 
+            WHERE 
+            -- article_show='yes' 
+            -- AND 
+            phpkb_relations.category_id = {category_id} 
             AND phpkb_relations.article_id = phpkb_articles.article_id
             """)
     
     articles = c.fetchall()
-    global TOTAL_PAGES_CLONED
+    global TOTAL_PAGES_UPDATED
     pages = 0
     for id, content, title in articles:
         sanitizedTitle=sanitize_filename(title)
         filename = f"{id} - {sanitizedTitle}"
-        print ('    Cloning article: ' + filename)
+        print ('    Updating article: ' + filename)
         pages += 1
-        newArticleId = cloneArticle(id, category_id, newCategoryId)
-        ARTICLE_MAPPING.update({id:newArticleId})
-        updateMappingJson()
-    TOTAL_PAGES_CLONED += pages
-    print(f"\nCloned {pages} articles, total {TOTAL_PAGES_CLONED}\n\n-----\n")
+        updateArticleLinks(id)
+    TOTAL_PAGES_UPDATED += pages
+    print(f"\nUpdated {pages} articles, total {TOTAL_PAGES_UPDATED}\n\n-----\n")
     return pages
 
-def cloneArticle(article_id, category_id, newCategoryId):
+
+def updateArticleLinks(article_id):
     
-    c = CONNECTION.cursor(buffered=True)    
+    c = CONNECTION.cursor() #(buffered=True)
+    c.execute(f"""
+            SELECT article_content from phpkb_articles WHERE article_id={article_id};
+            """)
     
-    if not article_id in ARTICLE_MAPPING:
-        c.execute(f"""
-                CREATE TEMPORARY TABLE tmp SELECT * from phpkb_articles WHERE article_id={article_id};
-                """)
-        c.execute(f"""
-                ALTER TABLE tmp DROP article_id;
-                """)
-        c.execute(f"""
-                INSERT INTO phpkb_articles SELECT 0,tmp.* FROM tmp;
-                """)
-        c.execute(f"""
-                DROP TABLE tmp;
-                """)
-        c.execute(f"""
-                SELECT MAX(article_id) FROM phpkb_articles;
-                """)
+    article_content = html.unescape(str(c.fetchone()[0]))
+      
+    for oldArticleId in MAPPING['Articles'].keys():
+    
+        newArticleId = MAPPING['Articles'][oldArticleId]
         
-        newArticleId = str(c.fetchone()[0])
-    else:
-        newArticleId = ARTICLE_MAPPING[article_id]
-    
-    print(newArticleId)
+        pattern = re.compile(fr'data-value="{oldArticleId}"', flags=re.MULTILINE)
+        replacementRegex = fr'data-value="{newArticleId}"'
+        if len(re.findall(pattern, article_content))>0:
+            article_content = re.sub(pattern, replacementRegex, article_content)
+            print(f'Replaced article markers {oldArticleId} : {newArticleId}')
 
-    c.execute(f"""
-            SELECT article_priority from phpkb_relations 
-            WHERE article_id={article_id}
-            AND category_id={category_id};
-            """)
-    
-    article_priority = str(c.fetchone()[0])
-    print(article_priority)
-    
-    c.execute(f"""
-            INSERT INTO phpkb_relations (article_id, category_id, article_priority)
-            VALUES ({newArticleId}, {newCategoryId}, {article_priority});
-            """)
 
-    return newArticleId
+        pattern = re.compile(fr'Article-ID:{oldArticleId}', flags=re.MULTILINE)
+        replacementRegex = fr'Article-ID:{newArticleId}'
+        if len(re.findall(pattern, article_content))>0:
+            article_content = re.sub(pattern, replacementRegex, article_content)
+            print(f'Replaced article markers {oldArticleId} : {newArticleId}')
 
-def cloneCategory(category_id, newParentId):
-    c = CONNECTION.cursor(buffered=True)
-    c.execute(f"""
-            CREATE TEMPORARY TABLE tmp SELECT * from phpkb_categories WHERE category_id={category_id};
-            """)
-    c.execute(f"""
-            ALTER TABLE tmp DROP category_id;
-            """)
-    c.execute(f"""
-            UPDATE tmp SET parent_id ={newParentId};
-            """)
-    c.execute(f"""
-            INSERT INTO phpkb_categories SELECT 0,tmp.* FROM tmp;
-            """)
-    c.execute(f"""
-            DROP TABLE tmp;
-            """)
-    c.execute(f"""
-            SELECT MAX(category_id) FROM phpkb_categories;
-            """)
+        pattern = re.compile(fr'article\.php\?id={oldArticleId}', flags=re.MULTILINE)
+        replacementRegex = fr'article\.php\?id={newArticleId}'
+        if len(re.findall(pattern, article_content))>0:
+            article_content = re.sub(pattern, replacementRegex, article_content)
+            print(f'Replaced article links {oldArticleId} : {newArticleId}')
+        
+    for oldCategoryId in MAPPING['Categories'].keys():
     
-    newCategoryId = str(c.fetchone()[0])
+        newCategoryId = MAPPING['Categories'][oldCategoryId]
+
+        pattern = re.compile(fr'category\.php\?id={oldCategoryId}', flags=re.MULTILINE)
+        replacementRegex = fr'category\.php\?id={newCategoryId}'
+        if len(re.findall(pattern, article_content))>0:
+            article_content = re.sub(pattern, replacementRegex, article_content)
+            print(f'Replaced category links {oldCategoryId} : {newCategoryId}')
     
-    print(newCategoryId)
+    pattern = re.compile(fr'Comindware Business Application Platform', flags=re.MULTILINE)
+    replacementRegex = fr'Comindware Platform'
+    if len(re.findall(pattern, article_content))>0:
+        article_content = re.sub(pattern, replacementRegex, article_content)
+        print(f'Replaced product name with Comindware Platform')
+        
+    pattern = re.compile(fr'(Comindware )?Architect', flags=re.MULTILINE)
+    replacementRegex = fr'«Архитектор»'
+    if len(re.findall(pattern, article_content))>0:
+        article_content = re.sub(pattern, replacementRegex, article_content)
+        print(f'Replaced Comindware Architect with «Архитектор»')
+    
+    # # print(f'Updating links in article {article_id}')
+    
+    article_content = html.escape(article_content)
 
-    return newCategoryId
-
+   
+    try:
+        print("execution", len(article_content))
+        c.execute("""
+                UPDATE phpkb_articles 
+                SET article_content=%s 
+                WHERE article_id=%s;
+                """, (article_content, article_id))
+    except:
+        print("Couldn't update the article")
+        exit()
+    
 def fetchCategories(show='yes', status='public', language_id=2, parent_id=''):
 
     c = CONNECTION.cursor()    
@@ -162,9 +155,11 @@ def fetchCategories(show='yes', status='public', language_id=2, parent_id=''):
     c.execute(f"""
             SELECT DISTINCT category_id, category_name, parent_id
             FROM phpkb_categories 
-            WHERE category_show='yes' 
-            AND category_status = 'public'
-            AND phpkb_categories.language_id = 2
+            WHERE 
+            -- category_show = '{show}' 
+            -- AND 
+            category_status = '{status}'
+            AND phpkb_categories.language_id = {language_id}
             AND parent_id = '{parent_id}'
             """)
 
@@ -179,6 +174,14 @@ def listCategories(categories):
         index += 1
 
 def main():
+    
+    global MAPPING
+    
+    MAPPING = loadMappingJson()
+    
+    if len(MAPPING) == 0:
+        print('Empty .mapping.json')
+        exit()
     
     with open(".serverCredentials.json", "r") as serverCredentialsFile: 
         
@@ -216,14 +219,14 @@ def main():
         database = sql_database
     )
     
-    cloneChildren = ''
-    categoryId = ''
+    updateChildren = ''
+    categoryId = '298'
     parent_category = ''
     categoryChoice = ''
 
     print('\nRoot categories:\n')
 
-    while cloneChildren != 'y':
+    while updateChildren != 'y':
         
         
         categoryChoice = ''
@@ -234,9 +237,9 @@ def main():
             listCategories(categories)
             print("\n---------\n")
             
-            if cloneChildren.isnumeric() and int(cloneChildren) <= len(categories):
-                    categoryChoice = int(cloneChildren)-1
-                    cloneChildren = 'y'
+            if updateChildren.isnumeric() and int(updateChildren) <= len(categories):
+                    categoryChoice = int(updateChildren)-1
+                    updateChildren = 'y'
             else:    
                 while not (categoryChoice.isnumeric() and int(categoryChoice) <= len(categories)):
                     categoryChoice = input("Choose category to browse (1 to {}): ".format(len(categories)))
@@ -257,32 +260,29 @@ def main():
             if childrenCategoriesNumber > 0:
                 print ('\nIt has {} child categories:\n'.format(childrenCategoriesNumber))
                 listCategories(childrenCategories)
-                cloneChildren = input("\nEnter `Y` to clone the category and all its child categories and articles. \n Or choose a category to browse (1 to {}). \n".format(childrenCategoriesNumber)).lower()
+                updateChildren = input("\nEnter `Y` to update the category and all its child categories and articles. \n Or choose a category to browse (1 to {}). \n".format(childrenCategoriesNumber)).lower()
             else: 
                 print ('\nIt has no child categories')
-                cloneChildren = input("\nEnter `Y` to clone the category and all articles from this category. ".format(categoryId, categoryTitle)).lower()
-                if cloneChildren !='y': 
-                    print('Cloned nothing')
+                updateChildren = input("\nEnter `Y` to update the category and all articles from this category. ".format(categoryId, categoryTitle)).lower()
+                if updateChildren !='y': 
+                    print('Updated nothing')
                     break
 
     else:
         if categories[categoryChoice]:
-            # cloneParent = input("\nEnter `Y` to clone the parent category itself along with its children. ".format(categoryId, categoryTitle)).lower() == 'y'
-            cloneCategoryChildren(categories[categoryChoice])
+            updateCategoryChildren(categories[categoryChoice])
         
     
     CONNECTION.close()
     server.close()
     server.stop()
-    updateMappingJson()
+       
+def loadMappingJson():
 
-def updateMappingJson():
-    MAPPING.update({'Categories':CATEGORY_MAPPING, 'Articles':ARTICLE_MAPPING})
-    #if input('Save mapping? Y / N\n').lower() == 'y':
-    with open(".mapping.json", "w") as mappingFile: 
-        mappingJson = json.dumps(MAPPING, indent = 4, ensure_ascii=False)
-        print(mappingJson)
-        mappingFile.write(mappingJson)
+    with open(".mapping.json", "r") as mappingJsonFile: 
+        mappingJsonFileContent = mappingJsonFile.read()
+        mappingJson = json.loads(mappingJsonFileContent) if mappingJsonFileContent else dict()
+        return mappingJson
 
 if __name__ == "__main__":
     main()

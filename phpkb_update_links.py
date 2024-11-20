2dd03206122275a3faaea6944e21c1231f954fac
Arterm Sedov <arterm.sedov@gmail.com>
2024-11-20 19:16:12 +0300

    Updated phpkb_update_links.py

--- phpkb_update_links.py
+++ phpkb_update_links.py
@@ -38,9 +38,9 @@ def updateCategoryChildren(parent):
         FROM phpkb_categories 
         WHERE 
         -- category_show='yes' 
+        -- AND category_status = 'public'
         -- AND 
-        category_status = 'public'
-        AND phpkb_categories.language_id = 2
+        phpkb_categories.language_id = 2
         AND parent_id = {id}
         """)
     children = c.fetchall()
@@ -80,12 +80,18 @@ def updateArticlesInCategory (category_id):
 
 def updateArticleLinks(article_id):
     
+    changesMade = False
+    
     c = CONNECTION.cursor() #(buffered=True)
     c.execute(f"""
-            SELECT article_content from phpkb_articles WHERE article_id={article_id};
+            SELECT article_content, article_title from phpkb_articles WHERE article_id={article_id};
             """)
     
-    article_content = html.unescape(str(c.fetchone()[0]))
+    result = c.fetchone()
+           
+    article_content = html.unescape((result[0]))
+    
+    article_title = result[1]
       
     for oldArticleId in MAPPING['Articles'].keys():
     
@@ -94,6 +100,7 @@ def updateArticleLinks(article_id):
         pattern = re.compile(fr'data-value="{oldArticleId}"', flags=re.MULTILINE)
         replacementRegex = fr'data-value="{newArticleId}"'
         if len(re.findall(pattern, article_content))>0:
+            changesMade = True
             article_content = re.sub(pattern, replacementRegex, article_content)
             print(f'Replaced article markers {oldArticleId} : {newArticleId}')
 
@@ -101,12 +108,14 @@ def updateArticleLinks(article_id):
         pattern = re.compile(fr'Article-ID:{oldArticleId}', flags=re.MULTILINE)
         replacementRegex = fr'Article-ID:{newArticleId}'
         if len(re.findall(pattern, article_content))>0:
+            changesMade = True
             article_content = re.sub(pattern, replacementRegex, article_content)
             print(f'Replaced article markers {oldArticleId} : {newArticleId}')
 
         pattern = re.compile(fr'article\.php\?id={oldArticleId}', flags=re.MULTILINE)
         replacementRegex = fr'article\.php\?id={newArticleId}'
         if len(re.findall(pattern, article_content))>0:
+            changesMade = True
             article_content = re.sub(pattern, replacementRegex, article_content)
             print(f'Replaced article links {oldArticleId} : {newArticleId}')
         
@@ -117,36 +126,66 @@ def updateArticleLinks(article_id):
         pattern = re.compile(fr'category\.php\?id={oldCategoryId}', flags=re.MULTILINE)
         replacementRegex = fr'category\.php\?id={newCategoryId}'
         if len(re.findall(pattern, article_content))>0:
+            changesMade = True
             article_content = re.sub(pattern, replacementRegex, article_content)
             print(f'Replaced category links {oldCategoryId} : {newCategoryId}')
     
     pattern = re.compile(fr'Comindware Business Application Platform', flags=re.MULTILINE)
     replacementRegex = fr'Comindware Platform'
     if len(re.findall(pattern, article_content))>0:
+        changesMade = True
         article_content = re.sub(pattern, replacementRegex, article_content)
-        print(f'Replaced product name with Comindware Platform')
+        print(f'Replaced product name with Comindware Platform in article content')
+
+    if len(re.findall(pattern, article_title))>0:
+        changesMade = True
+        article_title = re.sub(pattern, replacementRegex, article_title)
+        print(f'Replaced product name with Comindware Platform in article title')
         
     pattern = re.compile(fr'(Comindware )?Architect', flags=re.MULTILINE)
     replacementRegex = fr'«Архитектор»'
     if len(re.findall(pattern, article_content))>0:
+        changesMade = True
         article_content = re.sub(pattern, replacementRegex, article_content)
         print(f'Replaced Comindware Architect with «Архитектор»')
+        
+    pattern = re.compile(fr'4\.7', flags=re.MULTILINE)
+    replacementRegex = fr'5.0'
+    if len(re.findall(pattern, article_title))>0:
+        newArticle_title = re.sub(pattern, replacementRegex, article_title)
+        if input(f'Replace {article_title} with {newArticle_title} ? Y/N').lower() == 'y':
+            changesMade = True
+            article_title = newArticle_title
+            print(f'Replaced 4.7 with 5.0 in article title: {article_title}')
+    
+    
+    pattern = re.compile(fr'(^.*)(4\.7)(.*$)', flags=re.MULTILINE)
+    replacementRegex = fr'\g<1>5.0\g<3>'
+    for result in re.finditer(pattern, article_content):
+        foundLine = result.group(0)
+        print(f'Found line: {foundLine}')
+        if input(f'Replace 4.7 with 5.0 ? Y/N').lower() == 'y':
+            changesMade = True
+            replacedLine = re.sub(pattern, replacementRegex, foundLine)
+            article_content = re.sub(pattern, replacementRegex, article_content, count=1)
+            print(f'Replaced 4.7 with 5.0 in article content: {replacedLine}')
     
     # # print(f'Updating links in article {article_id}')
     
     article_content = html.escape(article_content)
-
-   
-    try:
-        print("execution", len(article_content))
-        c.execute("""
-                UPDATE phpkb_articles 
-                SET article_content=%s 
-                WHERE article_id=%s;
-                """, (article_content, article_id))
-    except:
-        print("Couldn't update the article")
-        exit()
+      
+    if changesMade:
+        try:
+            print("Executing SQL")
+            c.execute("""
+                    UPDATE phpkb_articles 
+                    SET article_title=%s,
+                    article_content=%s 
+                    WHERE article_id=%s;
+                    """, (article_title, article_content, article_id))
+        except:
+            print("Couldn't update the article")
+            exit()
     
 def fetchCategories(show='yes', status='public', language_id=2, parent_id=''):
 
@@ -157,9 +196,9 @@ def fetchCategories(show='yes', status='public', language_id=2, parent_id=''):
             FROM phpkb_categories 
             WHERE 
             -- category_show = '{show}' 
+            -- AND category_status = '{status}'
             -- AND 
-            category_status = '{status}'
-            AND phpkb_categories.language_id = {language_id}
+            phpkb_categories.language_id = {language_id}
             AND parent_id = '{parent_id}'
             """)
 
@@ -220,7 +259,7 @@ def main():
     )
     
     updateChildren = ''
-    categoryId = '298'
+    categoryId = ''
     parent_category = ''
     categoryChoice = ''
 
